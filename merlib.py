6aa19916efa3b5f135d4db41d871e80682aadedf
benitesf <edzon_bf@hotmail.com>
2021-01-29 17:00:34 +0100

    add directory management

--- merlib.py
+++ merlib.py
@@ -6,7 +6,9 @@ Created on Mon Dec 28 18:49:26 2020
 @author: joker
 """
 
+import datetime
 import logging
+import os
 
 from spider import MerlibSpider
 
@@ -17,10 +19,19 @@ mls = MerlibSpider()
 #cat = mls.get_categories()
 mls.load_categories(category_fpath)
 keys = ["MPE1246"]
-data = mls.extract_items_info(keys=keys, all_keys=False)
+
+outdir = os.path.join(os.getcwd(), "data", "raw")
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+date = datetime.datetime.now().strftime("%d-%m-%Y")
+filename = os.path.join(outdir, f"MPE1246_{date}.data")
+
+with open(filename, 'ab+') as fp:
+    mls.extract_items_info(fp, keys=keys, all_keys=False)
 
 
 """
-i`f __name__ == "__main__":
+if __name__ == "__main__":
     
 """      
